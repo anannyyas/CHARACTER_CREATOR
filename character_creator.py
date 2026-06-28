2ea5d5c463f382a2c615c702b77cb0d86f644aa1
Ananya <ananyaas605@gmail.com>
2026-06-28 16:16:15 +0530

    Add files via upload

---
+++ character_creator.py
@@ -0,0 +1,40 @@
+def create_character(character_name, strength, intelligence, charisma):
+
+    full_dot = "●"
+    empty_dot = "○"
+
+    if not isinstance(character_name, str):
+        return "The character name should be a string"
+
+    if character_name == "":
+        return "The character should have a name"
+
+    if len(character_name) > 10:
+        return "The character name is too long"
+
+    if " " in character_name:
+        return "The character name should not contain spaces"
+
+    if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
+        return "All stats should be integers"
+
+    if strength < 1 or intelligence < 1 or charisma < 1:
+        return "All stats should be no less than 1"
+
+    if strength > 4 or intelligence > 4 or charisma > 4:
+        return "All stats should be no more than 4"
+
+    if strength + intelligence + charisma != 7:
+        return "The character should start with 7 points"
+
+ 
+    str_bar = full_dot * strength + empty_dot * (10 - strength)
+    int_bar = full_dot * intelligence + empty_dot * (10 - intelligence)
+    cha_bar = full_dot * charisma + empty_dot * (10 - charisma)
+
+    return (
+    character_name + "\n"
+    "STR " + str_bar + "\n"
+    "INT " + int_bar + "\n"
+    "CHA " + cha_bar
+)
